e2852076521a8cecc4e4079d5c70c5cff6d81791
ismail-efe16 <ismailefe1881@gmail.com>
2026-04-16 23:20:57 +0300

    Queen Methods implemented
    Combined the already defined methods of Bishop and Rook classes to create the Queen class's methods.

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -4,23 +4,35 @@ import chess.ActionRequest;
 import chess.Cell;
 import chess.Player;
 
-public class Queen extends Piece{
+public class Queen extends Piece {
 
     public Queen(Cell currentlyOnTheCell, String name, Player belongsToOwner) {
         super(currentlyOnTheCell, name, belongsToOwner);
-        //TODO Auto-generated constructor stub
+
     }
 
     @Override
     public ActionRequest canItMove(Cell toCell) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'canItMove'");
+        Bishop bishop = new Bishop(this.getCurrentlyOnTheCell(), "temporaryBishop", this.getBelongsToOwner());
+        Rook rook = new Rook(this.getCurrentlyOnTheCell(), "temporaryRook", this.getBelongsToOwner());
+
+        return new ActionRequest("Queen moves",
+                bishop.canItMove(toCell).isSuccessful || rook.canItMove(toCell).isSuccessful);
     }
 
     @Override
     public int[][] getPossibleLocations() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPossibleLocations'");
+        Bishop bishop = new Bishop(this.getCurrentlyOnTheCell(), "temporaryBishop", this.getBelongsToOwner());
+        Rook rook = new Rook(this.getCurrentlyOnTheCell(), "temporaryRook", this.getBelongsToOwner());
+        int[][] BishopArray = bishop.getPossibleLocations();
+        int[][] RookArray = rook.getPossibleLocations();
+        int[][] QueenArray = new int[BishopArray.length + RookArray.length][2];
+        for (int i = 0; i < BishopArray.length; i++) {
+            QueenArray[i] = BishopArray[i];
+        }
+        for (int i = 0; i < RookArray.length; i++) {
+            QueenArray[i + BishopArray.length] = RookArray[i];
+        }
+        return QueenArray;
     }
-
 }
